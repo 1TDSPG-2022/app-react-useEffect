1967a14ca495be88b24bcd6957a8975274e64022
GuilhermeMenezesSilva <Guissilvamenezes@gmail.com>
2022-08-15 12:20:39 -0300

    Estudo com useEffect iniciado

---
+++ RM89384/aula03/src/app.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import Api from "./components/api/Api"
+
+export default function App(){
+    
+    const [valorState, setvalorState] = useState(0)
+    
+    useEffect(() =>{
+        console.log('Renderizou')
+    }, [valorState])
+
+    const [repositorios, setRepositorios] = useState([])
+    
+    useEffect(async () =>{
+        
+        const response = fetch("https://api.github.com/users/GuilhermeMenezesdaSilva/repos")
+        const data = await response.json()
+
+        setRepositorios(data)
+    }, [])
+
+    return(
+        <div>
+            <h1>Inicio dos estudos com UseEffects</h1>
+                        
+            <Api>
+            <h2>Lista de repositórios do GitHub</h2>
+            <p>listas</p>
+            <h1>Classes</h1>    
+            </Api>
+            <p>valor de State = {valorState}</p>
+            <button onClick={() => setvalorState(valorState + 1)}>Aumentar</button>
+
+            <div>
+                <h2>Repositorios</h2>
+                <ul>
+                    {repositorios.map((repo, i) => 
+                        <li key={i}>{repo.name}</li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    )
+}
