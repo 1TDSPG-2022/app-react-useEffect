235916e89663c41cbb0852ea1c931000988365bc
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-15 12:21:27 -0300

    Exemplos de useEffect com fetch e api pública

--- pf0670/aula03/src/App.jsx
+++ pf0670/aula03/src/App.jsx
@@ -1,17 +1,46 @@
-import Reac from "react"
+import React, { useEffect, useState } from "react"
 import Api from "./components/api/Api"
 
 export default function App() {
+
+    const [valorState, setvalorState] = useState(0)
+
+    useEffect(() => {
+        console.log('Renderizou!')
+    }, [valorState])
+
+
+    const [repositorios, setRepositorios] = useState([])
+
+    useEffect(async () => {
+
+        const response = await fetch("https://api.github.com/users/alecarlosjesus/repos")
+        const data = await response.json()
+
+        setRepositorios(data)
+
+    }, [])
+
     return (
         <div>
             <h1>Início dos Estudos com useEffects</h1>
-
 
             <Api>
                 <h2>Lista de Repositórios do GitHUB</h2>
                 <p>Listas</p>
                 <h1>Classes</h1>
             </Api>
+            <p>Valor de State = {valorState}</p>
+            <button onClick={() => setvalorState(valorState + 1)}>AUMENTAR</button>
+
+            <div>
+                <h2>Repositórios</h2>
+                <ul>
+                    {repositorios.map((repo, i) =>
+                        <li key={i}>{repo.name}</li>
+                    )}
+                </ul>
+            </div>
 
         </div>
     )
