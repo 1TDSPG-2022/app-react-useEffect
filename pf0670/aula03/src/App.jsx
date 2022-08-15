7e22032f17384f76c1efe905c6b209ff028556ab
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-15 11:39:34 -0300

    Exemplos de props.Childrens

---
+++ pf0670/aula03/src/App.jsx
@@ -0,0 +1,18 @@
+import Reac from "react"
+import Api from "./components/api/Api"
+
+export default function App() {
+    return (
+        <div>
+            <h1>Início dos Estudos com useEffects</h1>
+
+
+            <Api>
+                <h2>Lista de Repositórios do GitHUB</h2>
+                <p>Listas</p>
+                <h1>Classes</h1>
+            </Api>
+
+        </div>
+    )
+}
