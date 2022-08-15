bf23a3e68838d11fe416550f90f09d56d9b55452
evelywath <rm92970@fiap.com.br>
2022-08-15 12:28:53 -0300

    StateValue Fetch e Api

--- RM92970/AULA03/src/App.jsx
+++ RM92970/AULA03/src/App.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 import Api from ".components/api/Api"
+import {useEffect} from 'react'
+import {useState } from 'react'
 
 export default function App() {
+
+    const [valorState, setValorState] = useState()
+
+    useEffect(() => {
+        console.log('Renderizou!')
+    }, [valorState])
+
+    const [repositorios, setRepositorios] = useState([])
+
+    useEffect( async () => {
+        
+        const response = fetch("https://api.github.com/users/evelywath/repos")
+        const data  = await response.json()
+
+        setRepositorios(data)
+
+    }, [])
+
     return(
         <div>
             <h1>Início dos Estudos com useEffects</h1>
 
-        
             <Api>
             <h2>Lista de respositórios do GitHUB</h2>
             <p>Listas</p>
             <h1>Classes</h1>
             </Api>
-        
+            <p>Valor de State = {valorState}</p>
+            <button onClick={() => setValorState(valorState + 1)}>AUMENTAR</button>
+
+            <div>
+                <h2>Respositórios</h2>
+                <ul>
+                    {repositorios.map((repo,i))} =>
+                        <li key={i}>{repo.name}</li>
+                    )
+                </ul>
+            </div>
         </div>
     )
 }
