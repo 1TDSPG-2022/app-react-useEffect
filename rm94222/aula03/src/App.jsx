979f550d585f9d7f17e7354e83cb7b9513aa9545
lelemaia <rm94222@fiap.com.br>
2022-08-15 11:59:34 -0300

    ,

---
+++ rm94222/aula03/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+
+    const [valorState, setvalorState] = useState()
+    
+    return (
+        <div>
+            <h1>Iniciona dos Estudos com useEffect</h1>
+
+            <api>
+                <h2>Lista de Repositorios do GitHub</h2>
+                <p>Listas</p>
+                <h1>Classes</h1>
+            </api>
+        </div>
+    )
+}
